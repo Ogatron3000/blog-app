675b619c559f36583020c710f01e84e61884fd1a
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2020-09-14 00:23:42 +0200

    first commit

---
+++ client/src/components/DeleteWindow.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { Button, Modal } from 'react-bootstrap'
+
+const DeleteWindow = ({ show, handleShow, handleDelete }) => {
+	return (
+		<Modal show={show} onHide={handleShow} centered>
+			<Modal.Header closeButton>
+				<Modal.Title>Modal heading</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>Are you sure you want to delete this blog?</Modal.Body>
+			<Modal.Footer>
+				<Button variant="secondary" onClick={handleShow}>
+					Close
+				</Button>
+				<Button variant="danger" onClick={handleDelete}>
+					Yes
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	)
+}
+
+export default DeleteWindow
