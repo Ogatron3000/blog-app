15c9e5b844b1a9cb4f3f4d6284590ed9b67b6d13
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2020-09-15 22:38:34 +0200

    build path

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ if (process.env.NODE_ENV === 'test') {
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
